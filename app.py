1562352e8f471d3b96700130eef8a6c2e41c1cff
cjw <choij2015@gmail.com>
2024-06-05 13:40:03 +0900

    update

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 
-
 ''' 
 TODO: 
 get the list of user-chats by calling /api/userchats
@@ -15,6 +14,7 @@ from flask import Flask, redirect, render_template, request, flash, jsonify, sen
 from dotenv import load_dotenv
 import os
 import requests
+from collections import defaultdict
 
 app = Flask(__name__)
 
@@ -64,7 +64,7 @@ def get_chats() -> dict:
         "X-Access-Key": ACCESS_KEY,
         "X-Access-Secret": ACCESS_SECRET,
     }
-    response = requests.get("http://api.channel.io/open/v5/user-chats/", headers=headers, json=True)
+    response = requests.get("http://api.channel.io/open/v5/user-chats?sortOrder=desc&limit=30", headers=headers, json=True)
     return response.json()
 
 
@@ -76,6 +76,7 @@ def get_chat_messages(chatId: str) -> dict:
         "X-Access-Secret": ACCESS_SECRET,
     }
     response = requests.get("http://api.channel.io/open/v5/user-chats/" + chatId + "/messages?sortOrder=desc&limit=25", headers=headers, json=True)
+    response.encoding = "utf-8"
     return response.json()
 
 @app.route("/api/sessions/<chatId>", methods=["GET"])
@@ -86,25 +87,36 @@ def get_chat_sessions(chatId: str) -> dict:
         "X-Access-Secret": ACCESS_SECRET,
     }
     response = requests.get("http://api.channel.io/open/v5/user-chats/" + chatId + "/sessions", headers=headers, json=True)
+    response.encoding = "utf-8"
     return response.json()
 
 
-# @app.route("/api/<endpoint>")
-# def check_if_manager_exists_in_userchats() -> dict:
-#   return
+# 339530
+@app.route("/api/check/<managerId>")
+def check_if_manager_exists_in_userchats(managerId):
+    result = list()
+    _ids = list()
+    _userChats = get_chats()["userChats"]
+    for userChat in _userChats:
+        if managerId in userChat["managerIds"]:
+            # print(userChat["name"], userChat["id"])
+            _ids.append(userChat["id"])
+
+    for id in _ids:
+        _messages = get_chat_messages(id)["messages"]
+        for message in _messages:
+            if "blocks" in message and isinstance(message["blocks"], list):
+                for block in message["blocks"]:
+                    if "type" in block and block["type"] == "text" and "value" in block:
+                        result.append(block["value"])
 
 
 
 
-
-
-
-
-
+    return result
 
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
-
+    app.run(host='localhost', port=5010)
 
