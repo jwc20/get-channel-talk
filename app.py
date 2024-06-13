712c4dd0ecabbe189c3d383285f836d9e7e697cf
jwc20 <choij2015@gmail.com>
2024-06-13 21:31:32 +0900

    Refactor and optimize API endpoints for fetching user chats and chat messages

--- app.py
+++ app.py
@@ -1,41 +1,31 @@
-
-'''
-TODO:
-get the list of user-chats by calling /api/userchats
-loop over 'userChats', checking the 'managerIds' of each chat with the id we are checking
-    if the id is in the 'managerIds', get the chat messages from the 'userChats' by calling the '/api/messages/<chatId>' endpoint
-    else, skip
-
-'''
-
-
-
 from flask import Flask, redirect, render_template, request, flash, jsonify, send_file
 from dotenv import load_dotenv
 import os
 import requests
+from typing import DefaultDict, List, Dict
 from collections import defaultdict
 from datetime import datetime
 
 app = Flask(__name__)
 
 load_dotenv()
-
 ACCESS_KEY = os.getenv("ACCESS_KEY")
 ACCESS_SECRET = os.getenv("ACCESS_SECRET")
 
+# ---Helper Functions-----------------------------------
 
-# -Helper Functions-----------------------------------
 
 def convert_timestamp_to_date(timestamp: int) -> str:
-    '''
+    """
     input: timestamp in milliseconds
     output: date in the format 'Year-Month-Day Hour:Minute:Second'
-    '''
+    """
     date_time = datetime.utcfromtimestamp(timestamp / 1000)
-    return date_time.strftime('%Y-%m-%d %H:%M:%S')
+    return date_time.strftime("%Y-%m-%d %H:%M:%S")
 
-# ----------------------------------------------------
+
+# ------------------------------------------------------
+# ---Routes---------------------------------------------
 
 
 @app.route("/")
@@ -45,31 +35,25 @@ def index():
 
 @app.route("/api/<endpoint>", methods=["GET"])
 def get(endpoint: str) -> dict:
-    '''
+    """
     examples:
         endpoint = 'users'
         endpoint = 'managers'
         endpoint = 'managers/<manager_id>'
-    '''
+    """
     headers = {
         "Content-Type": "application/json",
         "X-Access-Key": ACCESS_KEY,
         "X-Access-Secret": ACCESS_SECRET,
     }
     print("http://api.channel.io/open/v5" + endpoint)
-    response = requests.get("http://api.channel.io/open/v5/" + endpoint, headers=headers, json=True)
+    response = requests.get(
+        "http://api.channel.io/open/v5/" + endpoint, headers=headers, json=True
+    )
     return response.json()
 
 
-@app.route("/api/userchats", methods=["GET"])
-def get_chats() -> dict:
-    headers = {
-        "Content-Type": "application/json",
-        "X-Access-Key": ACCESS_KEY,
-        "X-Access-Secret": ACCESS_SECRET,
-    }
-    response = requests.get("http://api.channel.io/open/v5/user-chats?sortOrder=desc&limit=100", headers=headers, json=True)
-    return response.json()["userChats"]
+
 
 
 @app.route("/api/messages/<chatId>", methods=["GET"])
@@ -79,9 +63,16 @@ def get_chat_messages(chatId: str) -> dict:
         "X-Access-Key": ACCESS_KEY,
         "X-Access-Secret": ACCESS_SECRET,
     }
-    response = requests.get("http://api.channel.io/open/v5/user-chats/" + chatId + "/messages?sortOrder=desc&limit=25", headers=headers, json=True)
+    response = requests.get(
+        "http://api.channel.io/open/v5/user-chats/"
+        + chatId
+        + "/messages?sortOrder=desc&limit=25",
+        headers=headers,
+        json=True,
+    )
     response.encoding = "utf-8"
     return response.json()
+
 
 @app.route("/api/sessions/<chatId>", methods=["GET"])
 def get_chat_sessions(chatId: str) -> dict:
@@ -90,41 +81,111 @@ def get_chat_sessions(chatId: str) -> dict:
         "X-Access-Key": ACCESS_KEY,
         "X-Access-Secret": ACCESS_SECRET,
     }
-    response = requests.get("http://api.channel.io/open/v5/user-chats/" + chatId + "/sessions", headers=headers, json=True)
+    response = requests.get(
+        "http://api.channel.io/open/v5/user-chats/" + chatId + "/sessions",
+        headers=headers,
+        json=True,
+    )
     response.encoding = "utf-8"
     return response.json()
 
 
 # 339530
-@app.route("/api/check/<managerId>")
-def check_if_manager_exists_in_userchats(managerId):
-    result = list()
-    _ids = list()
-    _userChats = get_chats()
+# @app.route("/api/check/<managerId>")
+# def check_if_manager_exists_in_userchats(managerId):
+#     result = collections.defaultdict(list)
+#     _ids = collections.defaultdict(list)
+#     states = "opened closed snoozed".split()
+
+#     _userChats = get_chats()
+#     for userChat in _userChats:
+#         # print(userChat)
+
+#         # if "managerIds" not in userChat:
+#         #     continue
+
+#         if managerId in userChat["managerIds"]:
+#             print(userChat["name"], userChat["id"])
+#             print(managerId, userChat["managerIds"])
+#             _ids.append(userChat["id"])
+
+#     for id in _ids:
+#         _messages = get_chat_messages(id)["messages"]
+#         for message in _messages:
+#             if "blocks" in message and isinstance(message["blocks"], list):
+#                 for block in message["blocks"]:
+#                     if "type" in block and block["type"] == "text" and "value" in block:
+#                         # result.append(block["value"])
+#                         result.append(message)
+#                         # print(convert_timestamp_to_date(message["createdAt"]), block["value"])
+
+#     return result
+
+@app.route("/api/userchats", methods=["GET"])
+def get_chats(
+    state: str = "opened", sort_order: str = "desc", limit: str = "25"
+) -> Dict[str, List[dict]]:
+    headers = {
+        "Content-Type": "application/json",
+        "X-Access-Key": ACCESS_KEY,
+        "X-Access-Secret": ACCESS_SECRET,
+    }
+
+    url = (
+        "http://api.channel.io/open/v5/user-chats?state="
+        + state
+        + "&sortOrder="
+        + sort_order
+        + "&limit="
+        + limit
+    )
+    
+    
+    # import pdb; pdb.set_trace()
+
+    response = requests.get(
+        url,
+        headers=headers,
+        json=True,
+    )
+    return response.json()["userChats"]
+
+@app.route("/api/managers/<manager_id>/chats/<state>/<limit>/<sort_order>", methods=["GET"])
+def check_if_manager_exists_in_userchats(manager_id: str, state: str, limit: str, sort_order: str) -> List[dict]:
+    result = defaultdict(list)
+    # _ids = defaultdict(list)
+    states = "all opened closed snoozed".split()
+
+    _userChats = get_chats(state=state, limit=limit, sort_order=sort_order)
+    print(_userChats)
+
     for userChat in _userChats:
         # print(userChat)
 
-        # if "managerIds" not in userChat:
+        # if "manager_ids" not in userChat:
         #     continue
 
-        if managerId in userChat["managerIds"]:
+        if manager_id in userChat["managerIds"]:
             print(userChat["name"], userChat["id"])
-            print(managerId, userChat["managerIds"])
-            _ids.append(userChat["id"])
+            print(manager_id, userChat["managerIds"])
+            # _ids.append(userChat["id"])
 
-    for id in _ids:
-        _messages = get_chat_messages(id)["messages"]
-        for message in _messages:
-            if "blocks" in message and isinstance(message["blocks"], list):
-                for block in message["blocks"]:
-                    if "type" in block and block["type"] == "text" and "value" in block:
-                        # result.append(block["value"])
-                        result.append(message)
-                        # print(convert_timestamp_to_date(message["createdAt"]), block["value"])
+    # for id in _ids:
+    #     _messages = get_chat_messages(id)["messages"]
+    #     for message in _messages:
+    #         if "blocks" in message and isinstance(message["blocks"], list):
+    #             for block in message["blocks"]:
+    #                 if "type" in block and block["type"] == "text" and "value" in block:
+    #                     # result.append(block["value"])
+    #                     result.append(message)
+    #                     # print(convert_timestamp_to_date(message["createdAt"]), block["value"])
 
     return result
 
 
+# ------------------------------------------------------
+
+
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='localhost', port=5010)
+    app.run(host="localhost", port=5010)
